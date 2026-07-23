a32fd30f56681d1487fac51237333f8e361274f5
Viraj Salunkhe <codewithmeu@gmail.com>
2026-07-23 13:02:03 +0530

    Configure production backend API

--- lib/utils/api_config.dart
+++ lib/utils/api_config.dart
@@ -1,18 +1,13 @@
-import 'package:flutter/foundation.dart';
-
 class ApiConfig {
-  // Base URL resolution based on platform
-  static String get baseUrl {
-    if (kIsWeb) {
-      return 'http://localhost:5000/api';
-    }
-    // Android Emulator host loopback address
-    return 'http://10.0.2.2:5000/api';
-  }
+  // Production backend deployed on Render
+  static const String baseUrl =
+      'https://task-management-app-backend-zb6m.onrender.com/api';
 
-  // Endpoints
+  // Authentication endpoints
   static String get registerUrl => '$baseUrl/auth/register';
   static String get loginUrl => '$baseUrl/auth/login';
   static String get getMeUrl => '$baseUrl/auth/me';
+
+  // Task endpoints
   static String get tasksUrl => '$baseUrl/tasks';
 }
